2fceac8b6e3500026db420ac4bda944344eed8d7
Raskolnikoff-D-503 <gaagevgenii@gmail.com>
2023-03-11 23:05:02 +0300

    rewrote the algorithm of tasks on board drag and drop, deleted unused files and constants

--- src/components/Planner/Planner.tsx
+++ src/components/Planner/Planner.tsx
@@ -1,17 +1,15 @@
 import React, {useState, DragEvent} from 'react';
-import {PlannerType, TaskDataType} from '@/types';
+import {PlannerConfigType, PlannerType, TaskDataType} from '@/types';
 import {Board, Task} from '@/components';
-import {getPlannerConfig} from '@/utils';
-import {mockData, plannerStructure} from './planner.constants';
+import {alternativeMockData, plannerStructure} from './planner.constants';
 
 import './Planner.scss';
 
 export const Planner = () => {
-  const [list, setList] = useState<TaskDataType[]>(mockData);
+  const [config, setConfig] = useState<PlannerConfigType>(alternativeMockData);
   const [currentTask, setCurrentTask] = useState<TaskDataType | null>(null);
   const [isDragOverTask, setIsDragOverTask] = useState<boolean>(false);
 
-  const config = getPlannerConfig(list);
   const configuratedData = plannerStructure.map((item) => ({...item, tasks: config[item.id]}));
 
   const handleDragStart = (data: TaskDataType) => (_: DragEvent<HTMLDivElement>) => {
@@ -30,54 +28,81 @@ export const Planner = () => {
     setIsDragOverTask(true);
   };
 
-  const handleDrop =
-    (data: TaskDataType, board: PlannerType) => (event: DragEvent<HTMLDivElement>) => {
-      event.preventDefault();
+  console.log(config);
 
-      setIsDragOverTask(false);
-      setList((current) =>
-        current.map((item) => {
-          console.log('test test', item);
+  const handleDrop = (data: TaskDataType) => (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
 
-          if (item.id === currentTask?.id) {
-            console.log('test item', {...item, order: data.order, type: data.type});
+    setIsDragOverTask(false);
 
-            return {...item, order: data.order, type: data.type};
-          }
+    if (currentTask) {
+      if (currentTask.type !== data.type) {
+        const currentBoard = [...config[currentTask.type]];
+        const board = [...config[data.type]];
 
-          if (item.id === data.id) {
-            return {
-              ...item,
-              order: item.type === currentTask?.type ? currentTask.order : board.tasks.length + 1,
-            };
-          }
+        const currentIndex = currentBoard.findIndex((item) => item.id === currentTask.id);
+        currentBoard.splice(currentIndex, 1);
 
-          return item;
-        }),
-      );
-    };
+        const dropIndex = board.findIndex((item) => item.id === data.id);
+        board.splice(dropIndex + 1, 0, {...currentTask, type: data.type});
+
+        setConfig((current) => ({
+          ...current,
+          [data.type]: board,
+          [currentTask.type]: currentBoard,
+        }));
+      } else {
+        const board = [...config[data.type]];
+
+        const currentIndex = board.findIndex((item) => item.id === currentTask.id);
+        board.splice(currentIndex, 1);
+
+        const dropIndex = board.findIndex((item) => item.id === data.id);
+        board.splice(dropIndex + 1, 0, currentTask);
+
+        setConfig((current) => ({
+          ...current,
+          [data.type]: board,
+        }));
+      }
+    }
+  };
 
   const handleBoardDragOver = (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
   };
 
-  const handleBoardDrop = (board: PlannerType) => (event: DragEvent<HTMLDivElement>) => {
+  const handleBoardDrop = (data: PlannerType) => (event: DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
-    if (!isDragOverTask) {
-      setList((current) =>
-        current.map((item) => {
-          if (item.id === currentTask?.id) {
-            return {
-              ...item,
-              order: board.id === currentTask.type ? currentTask.order : board.tasks.length + 1,
-              type: board.id,
-            };
-          }
+    if (!isDragOverTask && currentTask) {
+      if (currentTask.type !== data.id) {
+        const currentBoard = [...config[currentTask.type]];
+        const board = [...data.tasks];
 
-          return item;
-        }),
-      );
+        const currentIndex = currentBoard.findIndex((item) => item.id === currentTask.id);
+        currentBoard.splice(currentIndex, 1);
+
+        board.push({...currentTask, type: data.id});
+
+        setConfig((current) => ({
+          ...current,
+          [currentTask.type]: currentBoard,
+          [data.id]: board,
+        }));
+      } else {
+        const board = [...config[data.id]];
+
+        const currentIndex = board.findIndex((item) => item.id === currentTask.id);
+        board.splice(currentIndex, 1);
+
+        board.push({...currentTask, type: data.id});
+
+        setConfig((current) => ({
+          ...current,
+          [data.id]: board,
+        }));
+      }
     }
   };
 
@@ -98,7 +123,7 @@ export const Planner = () => {
                 onDragStart={handleDragStart(task)}
                 onDragLeave={handleDragLeave}
                 onDragOver={handleDragOver}
-                onDrop={handleDrop(task, board)}
+                onDrop={handleDrop(task)}
               />
             ))}
           </Board>
--- src/components/Planner/planner.constants.ts
+++ src/components/Planner/planner.constants.ts
@@ -1,4 +1,4 @@
-import {PlannerType, TaskDataType} from '@/types';
+import {PlannerConfigType, PlannerType} from '@/types';
 import {TASK_TYPE} from '@/constants';
 
 export const plannerStructure: PlannerType[] = [
@@ -19,10 +19,12 @@ export const plannerStructure: PlannerType[] = [
   },
 ];
 
-export const mockData: TaskDataType[] = [
-  {id: '1', content: 'Work on my schedule', type: TASK_TYPE.BACKLOG, order: 1},
-  {id: '2', content: 'Buy groceries', type: TASK_TYPE.BACKLOG, order: 3},
-  {id: '3', content: 'Code new project', type: TASK_TYPE.IN_PROGRESS, order: 1},
-  {id: '4', content: 'Code snake game', type: TASK_TYPE.DONE, order: 1},
-  {id: '5', content: 'Do workout', type: TASK_TYPE.BACKLOG, order: 2},
-];
+export const alternativeMockData: PlannerConfigType = {
+  BACKLOG: [
+    {id: '1', content: 'Work on my schedule', type: TASK_TYPE.BACKLOG},
+    {id: '5', content: 'Do workout', type: TASK_TYPE.BACKLOG},
+    {id: '2', content: 'Buy groceries', type: TASK_TYPE.BACKLOG},
+  ],
+  IN_PROGRESS: [{id: '3', content: 'Code new project', type: TASK_TYPE.IN_PROGRESS}],
+  DONE: [{id: '4', content: 'Code snake game', type: TASK_TYPE.DONE}],
+};
